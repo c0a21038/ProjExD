e73176da220ba2bfe03808d437732d27b5e42299
kawamurak <c0a21038ac@edu.teu.ac.jp>
2022-06-28 14:20:02 +0900

    練習2

--- Ex03/maze.py
+++ Ex03/maze.py
@@ -3,5 +3,10 @@ import tkinter as tk
 if __name__ == "__main__":
     maze = tk.Tk()
     maze.title("迷えるこうかとん")
-    maze.geometry("1500x900")
+    #maze.geometry("1500x900")
+    canvas = tk.Canvas(maze,
+                      width = 1500,
+                      height = 900,
+                      bg = "black")
+    canvas.pack()
     maze.mainloop()
