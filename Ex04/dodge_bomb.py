696eb56d0c116e482c3625e2596c1975e89a086e
kawamurak <c0a21038ac@edu.teu.ac.jp>
2022-07-05 15:28:14 +0900

    練習7

--- Ex04/dodge_bomb.py
+++ Ex04/dodge_bomb.py
@@ -32,7 +32,7 @@ def main():
     
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+        
         
 
         # 練習２
@@ -42,21 +42,43 @@ def main():
 
         # 練習４
         key_states = pg.key.get_pressed()
-        if key_states[pg.K_UP] == True:
-            kkimg_rct.centery -= 1
-        if key_states[pg.K_DOWN] == True:
-            kkimg_rct.centery += 1
-        if key_states[pg.K_RIGHT] == True:
-            kkimg_rct.centerx += 1
-        if key_states[pg.K_LEFT] == True:
-            kkimg_rct.centerx -= 1
+        if key_states[pg.K_UP] == True: kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN] == True: kkimg_rct.centery += 1
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        if key_states[pg.K_LEFT] == True: kkimg_rct.centerx -= 1
+        
+        if check_bound(kkimg_rct, screen_rct) != (1, 1): #領域外だったら
+            if key_states[pg.K_UP] == True: kkimg_rct.centery += 1
+            if key_states[pg.K_DOWN] == True: kkimg_rct.centery -= 1
+            if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
+            if key_states[pg.K_LEFT] == True: kkimg_rct.centerx += 1
+        
         screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         # 練習６
         bmimg_rct.move_ip(vx, vy)
-        
+        # 練習５
+        screen_sfc.blit(bmimg_sfc, bmimg_rct)
+        # 練習７
+        yoko, tate = check_bound(bmimg_rct, screen_rct)
+        vx *= yoko
+        vy *= tate
+
         pg.display.update()
         clock.tick(1000)
+
+# 練習７
+def check_bound(rct, scr_rct):
+    '''
+    [1] rct: こうかとん or 爆弾のRect
+    [2] scr_rct: スクリーンのRect
+    '''
+    yoko, tate = +1, +1 # 領域内
+    if rct.left < scr_rct.left or scr_rct.right < rct.right: yoko = -1 # 領域外
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
+    return yoko, tate
+
+
 
 if __name__ == "__main__":
     pg.init()
